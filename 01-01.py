97885cf891f0dcb0f3b2fcfec55d4d742590458d
raimaaan03 <43918793+raimaaan03@users.noreply.github.com>
2020-12-01 10:14:00 -0300

    Update 01-01.py

--- 01-01.py
+++ 01-01.py
@@ -4,9 +4,9 @@ solution = 0
 with open("01-input.txt", "r") as file:
     for line in file:
         line = int(line)
-        inputList.append(line)
         if (2020-line) in inputList:
             solution = line*(2020-line)
+        inputList.append(line)
 
 
 print(solution)
