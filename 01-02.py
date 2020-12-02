13c6f452ff34fef8ed2e831f2f88b07f563aa5fe
raimaaan03 <43918793+raimaaan03@users.noreply.github.com>
2020-12-02 11:57:33 -0300

    Update 01-02.py

--- 01-02.py
+++ 01-02.py
@@ -5,15 +5,10 @@ solution = 0
 with open("01-input.txt", "r") as file:
     for line in file:
         line = int(line)
-
         for i in inputList:
             twoSumList.append([line+i, line*i])
         inputList.append(line)
-#        print("\t"+str(line))
-#   print debugging is *fun*
         for first, second in twoSumList:
-#            print("\t\t" + str(line + a))
-#   print debugging is *really* fun
             if (line + first == 2020):
                 solution = line * second
 
