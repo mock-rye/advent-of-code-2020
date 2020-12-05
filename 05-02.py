48d005d327ec25eae946854084840b6009c28d5d
raimaaan03 <43918793+raimaaan03@users.noreply.github.com>
2020-12-05 03:07:29 -0300

    Create 05-02.py

---
+++ 05-02.py
@@ -0,0 +1,18 @@
+boolDict = {
+    'F': 0,
+    'B': 1,
+    'R': 1,
+    'L': 0
+}
+
+storage = set()
+
+with open("05-input.txt", "r") as file:
+    locMax = 0
+    for line in file:
+        string = ''.join(str(boolDict[char]) for char in line[0:-1]) #thanks cog
+        storage.add(int(string, 2))
+
+for i in range(min(storage), max(storage)):
+    if i not in storage:
+        print(i)
