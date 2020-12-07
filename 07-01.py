94237168def40dc8090b2332cf90954b34014821
raimaaan03 <43918793+raimaaan03@users.noreply.github.com>
2020-12-07 14:56:54 -0300

    Update 07-01.py

--- 07-01.py
+++ 07-01.py
@@ -4,26 +4,24 @@ holds = set()
 holdsnt = set()
 
 def checkFromNode(tree, node):
-    if "no other" in tree[node] or node in holdsnt:
+    children = tree[node]
+    if "no other" in children or node in holdsnt:
         holdsnt.add(node)
         return False
-    if "shiny gold" in tree[node] or node in holds:
+    if "shiny gold" in children or node in holds:
         holds.add(node)
         return True
-    if True in [checkFromNode(tree, n) for n in tree[node]]:
+    if True in [checkFromNode(tree, n) for n in children]:
         holds.add(node)
         return True
 
 with open("07-input.txt", "r") as file:
     for line in file:
-        line = [bit.strip() for bit in line.split('bags contain')]
-        contents = [elem.replace('.','') for elem in line[1].split(', ')]
-        contents = [elem.replace('bags','').replace('bag','') for elem in contents]
-        contents = [re.sub(r'\d+', '', elem) for elem in contents]
-        contents = [elem.strip() for elem in contents]
+        line = [elem.strip() for elem in line.split('bags contain')]
+        contents = [re.sub(r'bag(s?)|\d+|[.]','',elem).strip() for elem in line[1].split(',')]
+        #one-liner cleanup
         container = line[0]
-        tree[container] = set(contents)
-#        print(container, contents)
+        tree[container] = contents
 
 for key in tree:
     checkFromNode(tree, key)
