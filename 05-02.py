3ec88578ffa47a1ab7afc2c5846a71f752fb14ea
raimaaan03 <43918793+raimaaan03@users.noreply.github.com>
2020-12-07 15:04:29 -0300

    Update 05-02.py

--- 05-02.py
+++ 05-02.py
@@ -10,7 +10,7 @@ storage = set()
 with open("05-input.txt", "r") as file:
     locMax = 0
     for line in file:
-        string = ''.join(str(boolDict[char]) for char in line[0:-1]) #thanks cog
+        string = ''.join(str(boolDict[char]) for char in line.strip()) #thanks cog
         storage.add(int(string, 2))
 
 for i in range(min(storage), max(storage)):
