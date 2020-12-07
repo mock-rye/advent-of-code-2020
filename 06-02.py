a09740dca67ccdc910b129799c46f16f290c24fc
raimaaan03 <43918793+raimaaan03@users.noreply.github.com>
2020-12-07 15:19:26 -0300

    Update 06-02.py

--- 06-02.py
+++ 06-02.py
@@ -2,14 +2,14 @@ with open("06-input.txt", "r") as file:
     result = 0
     st = set()
     carryover = set("abcdefghijklmnopqrstuvwxyz")
-    for line in file:
-        line = line.strip()
-        if not line:
-            result = result + len(carryover)
-#            print(result, len(carryover), ''.join([elem for elem in sorted(carryover)]))
-            carryover = set("abcdefghijklmnopqrstuvwxyz")
-            continue
-        [st.add(char) for char in line]
-        carryover = carryover.intersection(st)
-        st.clear()
+    for group in file.read().split('\n\n'):
+        for line in group.split():
+#            print(line)
+            st.clear()
+            [st.add(char) for char in line]
+            carryover = carryover.intersection(st)
+#            print(carryover)
+        result = result + len(carryover)
+#        print()
+        carryover = set("abcdefghijklmnopqrstuvwxyz")
 print(result)
