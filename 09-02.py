a0c76078b148bece859657c4b75e8d119e1cb524
raimaaan03 <43918793+raimaaan03@users.noreply.github.com>
2020-12-09 15:57:01 -0300

    Update 09-02.py

--- 09-02.py
+++ 09-02.py
@@ -2,15 +2,16 @@ find = 22406676 # replace result from p1 here
 with open("09-input.txt", "r") as file:
     inp = file.read().strip().split('\n')
     numbers = [int(elem) for elem in inp]
-    sublist = list()
-    for lower in range(len(numbers)):
-        sublist.clear()
-        sublist.append(numbers[lower])
-        for upper in range(lower + 1, len(numbers)):
-            sublist.append(numbers[upper])
-            check = sum(sublist)
-            if check == find:
-                print(min(sublist) + max(sublist))
-                break
-            if check > find:
-                break
+    lower = 0
+    upper = 1
+    count = sum(numbers[0:2])
+    while count != find:
+# thanks bunny and cog
+        while count < find:
+            upper += 1
+            count += numbers[upper]
+        while count > find:
+            count -= numbers[lower]
+            lower += 1
+    sublist = numbers[lower:upper+1]
+    print(min(sublist) + max(sublist))
